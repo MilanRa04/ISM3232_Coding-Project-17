465f1e1f3202d44a8592c8377eaa58a4a18c3f8b
Milan Ranpariya <ranpariya04@gmail.com>
2024-11-24 14:37:51 -0500

    Setup root component with global state and routing.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Gallery from "./gallery.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Tour Comparison App</h1>
+      </header>
+      <main>
+        <Gallery />
+      </main>
+    </div>
+  );
+}
+
+export default App;
