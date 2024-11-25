2a8c9fc9a087fff726b6e23054e646941f8e96aa
Milan Ranpariya <ranpariya04@gmail.com>
2024-11-24 20:30:56 -0500

    Style components for better user experience.

--- src/gallery.jsx
+++ src/gallery.jsx
@@ -8,7 +8,7 @@ const Gallery = () => {
   useEffect(() => {
     const fetchTours = async () => {
       try {
-        const response = await fetch("https://course-api.com/react-tours-project");
+        const response = await fetch("https://www.course-api.com/react-tours-project");
         if (!response.ok) throw new Error("Failed to fetch data");
         const data = await response.json();
         setTours(data);
